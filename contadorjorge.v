e0bc9b78dfda915d8167ef97a776c52c1145a3b5
DKNS-Usal <jorgecuadradojcc@usal.es>
2022-12-04 16:30:53 +0100

    modified

--- contadorjorge.v
+++ contadorjorge.v
@@ -17,23 +17,23 @@ module JK(output reg Q,output wire nQ,input wire J,input wire K, input wire C);
 module contador(output wire[3:0]Q, input wire C);
     wire [3:0] nQ;  // Salida complementaria
 
-    wire AQ2Q3, AQ0nQ3, AnQ1nQ3, AnQ0nQ1,AQ0nQ2; // Salidas intermedias AND
-    wire OQ2Q3Q0nQ3,OQ2Q0,OnQ0nQ1Q0nQ2; // Salidas intermedias OR
+    wire AQ1Q0, AQ3nQ0, AnQ2nQ0, AnQ3nQ2,AQ3nQ1; // Salidas intermedias AND
+    wire OQ1Q0Q3nQ0,OQ1Q3,OnQ3nQ2Q3nQ1; // Salidas intermedias OR
 
-    and A0 (AQ2Q3, Q[1], Q[0]); // AND Q2 y Q3
-    and A1 (AQ0nQ3, Q[3], nQ[0]); // AND Q0 y nQ3
-    and A2 (AnQ1nQ3, nQ[2], nQ[0]); // AND nQ1 y nQ3
-    and A3 (AnQ0nQ1, nQ[3], nQ[2]); // AND nQ0 y nQ1
-    and A4 (AQ0nQ2, Q[3], nQ[1]); // AND Q0 y nQ2
+    and A0 (AQ1Q0, Q[1], Q[0]); // AND Q2 y Q3
+    and A1 (AQ3nQ0, Q[3], nQ[0]); // AND Q0 y nQ3
+    and A2 (AnQ2nQ0, nQ[2], nQ[0]); // AND nQ1 y nQ3
+    and A3 (AnQ3nQ2, nQ[3], nQ[2]); // AND nQ0 y nQ1
+    and A4 (AQ3nQ1, Q[3], nQ[1]); // AND Q0 y nQ2
 
-    or O0 (OQ2Q3Q0nQ3, AQ2Q3, AQ0nQ3); // OR Q2Q3 y Q0nQ3
-    or O1 (OQ2Q0, Q[1], Q[3]); // OR Q2 y Q0
-    or O2 (OnQ0nQ1Q0nQ2, AnQ0nQ1, AQ0nQ2); // OR nQ0nQ1 y Q0nQ2
+    or O0 (OQ1Q0Q3nQ0, AQ1Q0, AQ3nQ0); // OR Q2Q3 y Q0nQ3
+    or O1 (OQ1Q3, Q[1], Q[3]); // OR Q2 y Q0
+    or O2 (OnQ3nQ2Q3nQ1, AnQ3nQ2, AQ3nQ1); // OR nQ0nQ1 y Q0nQ2
 
     JK jk3 (Q[3], nQ[3],Q[2],nQ[2],C); 
-    JK jk2 (Q[2], nQ[2],OQ2Q3Q0nQ3,OQ2Q0,C);
-    JK jk1 (Q[1], nQ[1],nQ[2],AnQ1nQ3, C);
-    JK jk0 (Q[0], nQ[0],OnQ0nQ1Q0nQ2,OQ2Q0,C);
+    JK jk2 (Q[2], nQ[2],OQ1Q0Q3nQ0,OQ1Q3,C);
+    JK jk1 (Q[1], nQ[1],nQ[2],AnQ2nQ0, C);
+    JK jk0 (Q[0], nQ[0],OnQ3nQ2Q3nQ1,OQ1Q3,C);
 
 endmodule
 
@@ -54,7 +54,7 @@ module test;
         Contador1.jk1.Q='b1; // Inicializa el contador en 1         =10
         Contador1.jk0.Q='b0; // Inicializa el contador en 0
 
-    $monitor($time,"Q=%b%b%b%b",Q[3],Q[2],Q[1],Q[0]); //Muestra en pantalla el valor de los registros
+    $monitor($time,"Q=%b%b%b%b=%d",Q[3],Q[2],Q[1],Q[0],Q); //Muestra en pantalla el valor de los registros
     #100 $finish;
     end
 
