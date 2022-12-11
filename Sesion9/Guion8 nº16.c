819db0404aeb13f71cd09782857742d35f0ae6f8
DKNS-Usal <jorgecuadradojcc@usal.es>
2022-12-11 15:23:29 +0100

    a

--- Sesion9/Guion8 nº16.c
+++ Sesion9/Guion8 nº16.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int suma (int); 
+
+void main (){
+    int n;
+    printf("Introduzca un numero: ");
+    scanf("%d", &n);
+    printf("La suma de los numeros hasta %d es %d", n, suma(n));
+}
+
 int suma (int n){
-    int suma,i;
+    int suma=0,i;
+    printf("%d",n);
     for (i=0; i<=n; i++){
         suma+=i;
     }
     return suma;
 }
-void main (){
-    int n;
-    printf("Introduzca un numero: ");
-    scanf("%d", &n);
-    n=suma;
-    printf("%d", n);
-}
-
 
